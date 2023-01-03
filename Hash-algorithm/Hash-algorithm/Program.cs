a0755ca220b648def5104a247acc8c8f114c2cd5
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2023-01-03 20:32:22 +0200

    first commit

---
+++ Hash-algorithm/Hash-algorithm/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Hash_algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
