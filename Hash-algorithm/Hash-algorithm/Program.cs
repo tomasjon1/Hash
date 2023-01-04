d5a96ff641ff5a1afba59868825de27602549aec
tomasjon1 <72403454+tomasjon1@users.noreply.github.com>
2023-01-04 23:50:39 +0200

    added options for cmd line

--- Hash-algorithm/Hash-algorithm/Program.cs
+++ Hash-algorithm/Hash-algorithm/Program.cs
@@ -13,11 +13,65 @@ namespace Hash_algorithm
         {
             TestingService _testingService = new TestingService();
 
-            //_testingService.OutputLenghtTest();
-            //_testingService.OutputEqualityTest();
-            //_testingService.OutputCollisionTest();
-            //_testingService.OutputAvalancheTest();
-            _testingService.SpeedTest();
+            List<string> arguments = args.ToList();
+            string inputParam = "", outputParam = "", settingsParam = "";
+
+            //if (arguments.First().Contains("-in"))
+            //{
+            //    inputParam = arguments.First();
+            //    arguments.Remove(inputParam);
+            //}
+
+            //if (arguments.First().Contains("-out"))
+            //{
+            //    outputParam = arguments.First();
+            //    arguments.Remove(outputParam);
+            //}
+
+            //if (arguments.First().Contains("-t"))
+            //{
+            //    Console.WriteLine("Running tests..");
+            //    //_testingService.OutputLenghtTest();
+            //    //_testingService.OutputEqualityTest();
+            //    //_testingService.OutputCollisionTest();
+            //    //_testingService.OutputAvalancheTest();
+            //    //_testingService.SpeedTest();
+            //}
+
+            if (arguments.First().Contains("-in"))
+            {
+                inputParam = arguments.First();
+                arguments.Remove(inputParam);
+                Console.WriteLine($"INPUT {inputParam}");
+            }
+            
+
+            if (arguments.First().Contains("-out"))
+            {
+                outputParam = arguments.First();
+                arguments.Remove(outputParam);
+                Console.WriteLine($"OUTPUT {outputParam}");
+            }
+          
+
+            if (arguments.First().Contains("-"))
+            {
+                settingsParam = arguments.First();
+                arguments.Remove(settingsParam);
+                Console.WriteLine($"SETTING {settingsParam}");
+            }
+
+            if (settingsParam == "-t")
+            {
+                Console.WriteLine("Running tests..");
+                //_testingService.OutputLenghtTest();
+                //_testingService.OutputEqualityTest();
+                //_testingService.OutputCollisionTest();
+                //_testingService.OutputAvalancheTest();
+                //_testingService.SpeedTest();
+            }
+
+
         }
     }
 }
